00d18254b15ff7ba3eeda828d359a5828e42c7e0
Azeroz <nguoichet001@yahoo.com>
2015-04-14 11:26:44 +0700

    Using semantic-ui

--- index.php
+++ index.php
@@ -4,10 +4,13 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>Login System</title>
-	<link rel="stylesheet" href="assets/semantic.css">
-	<link rel="stylesheet" href="assets/main.css">
+	<link rel="stylesheet" href="assets/ui/semantic.css">
+	<link rel="stylesheet" href="assets/css/main.css">
 </head>
 <body>
-	
+	<div class="ui twitter button">
+		<i class="twitter icon"></i>
+			Twitter
+	</div>
 </body>
 </html>
