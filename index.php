ff8c8975f2c0d6f4a7e2ccf9a9dfab73edba30ca
Azeroz <nguoichet001@yahoo.com>
2015-04-14 17:33:01 +0700

    Update layout

--- index.php
+++ index.php
@@ -8,6 +8,10 @@
 	<link rel="stylesheet" href="assets/css/main.css">
 </head>
 <body>
+	<div class="container">
+		
+	</div>
+
 	<div class="ui twitter button">
 		<i class="twitter icon"></i>
 			Twitter
@@ -33,5 +37,13 @@
 		<i class="github icon"></i>
 			Github
 	</div>
+
+	
+
+	
+
+
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script src="assets/ui/semantic.js"></script>
 </body>
 </html>
