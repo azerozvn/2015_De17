7e47b8148215b21e3498e42b0beeeda61203b92c
Azeroz <nguoichet001@yahoo.com>
2015-04-15 09:29:39 +0700

    Layout updated

--- index.php
+++ index.php
@@ -4,70 +4,82 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>Login System</title>
+	<link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,700,300&subset=latin,vietnamese' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="assets/ui/semantic.css">
 	<link rel="stylesheet" href="assets/css/main.css">
 </head>
-<body>
+<body class="vertical-center">
 	<div class="container">
 		<div class="ui grid">
+			<div class="row">
+				<div class="ui header centered">
+					<h1 class="ui header">Login via your Social Account</h1>
+					<div class="sub header">A simple social network login system</div>
+				</div>
+			</div>
+			<div class="ui horizontal divider">pick one</div>
 			<div class="two column row">
 				<div class="column">
-					<div class="centered">
+					<div class="login-box centered">
 						<div class="ui twitter button">
 							<i class="twitter icon"></i>
 								Twitter
 						</div>
+						<div class="subtitle">Login via Twitter</div>
 					</div>	
 				</div>
 				<div class="column">
-					<div class="centered">
+					<div class="login-box centered">
 						<div class="ui flickr button">
 							<i class="flickr icon"></i>
 								Flickr
 						</div>
+						<div class="subtitle">Login via Flickr</div>
 					</div>	
 				</div>
 			</div>
 			<div class="two column row">
 				<div class="column">
-					<div class="centered">
+					<div class="login-box centered">
 						<div class="ui dropbox button">
 							<i class="dropbox icon"></i>
 								Dropbox
 						</div>
+						<div class="subtitle">Login via Dropbox</div>
 					</div>	
 				</div>
 				<div class="column">
-					<div class="centered">
+					<div class="login-box centered">
 						<div class="ui yahoo button">
 							<i class="yahoo icon"></i>
 								Yahoo
 						</div>
+						<div class="subtitle">Login via Yahoo</div>
 					</div>	
 				</div>
 			</div>
 			<div class="two column row">
 				<div class="column">
-					<div class="centered">
+					<div class="login-box centered">
 						<div class="ui github button">
 							<i class="github icon"></i>
 								Github
 						</div>
+						<div class="subtitle">Login via Github</div>
 					</div>	
 				</div>
 				<div class="column">
-					<div class="centered">
+					<div class="login-box centered">
 						<div class="ui foursquare button">
 							<i class="foursquare icon"></i>
 								Foursquare
 						</div>
+						<div class="subtitle">Login via Foursquare</div>
 					</div>	
 				</div>
 			</div>
 		</div>
 	</div>
-	
-
 
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 	<script src="assets/ui/semantic.js"></script>
