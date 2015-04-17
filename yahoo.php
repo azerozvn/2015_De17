1b3b051f19bb41db332092ecd6e7663b2dad1874
Azeroz <nguoichet001@yahoo.com>
2015-04-17 14:45:19 +0700

    Update to OAuth2

--- yahoo.php
+++ yahoo.php
@@ -3,7 +3,7 @@ require __DIR__.'/vendor/autoload.php';
 
 use OAuth\Common\Consumer\Credentials;
 use OAuth\Common\Http\Uri\UriFactory;
-use OAuth\OAuth1\Service\Yahoo;
+use OAuth\OAuth2\Service\Yahoo;
 use OAuth\Common\Storage\Session;
 use OAuth\ServiceFactory;
 
@@ -17,8 +17,8 @@ $currentUri->setQuery('');
 
 $servicesCredentials = [
     'yahoo' => [
-        'key'    => 'dj0yJmk9Unc0R0ZvWXRHWkV1JmQ9WVdrOVZtbEthVGRFTnpRbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD0xOA--',
-        'secret' => 'a1ca9f7f4a5b728fc9054a1b0ad6561d62f91ed8'
+        'key'    => 'dj0yJmk9MmFZTVpkRmM4TW9ZJmQ9WVdrOWRHeHJhMlpKTjJFbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD00OQ--',
+        'secret' => 'c5c91c17419c994dbf6c423a4feefca3781a18a2'
     ]
 ];
 
@@ -32,22 +32,16 @@ $credentials = new Credentials(
 );
 // Instantiate the Yahoo service using the credentials, http client and storage mechanism for the token
 $serviceFactory = new ServiceFactory();
-$yahooService = $serviceFactory->createService('Yahoo', $credentials, $storage);
-if (!empty($_GET['oauth_token'])) {
-    $token = $storage->retrieveAccessToken('Yahoo');
+$yahooService = $serviceFactory->createService('Yahoo', $credentials, $storage, array());
+if (!empty($_GET['code'])) {
+    $token = $yahooService->requestAccessToken($_GET['code']);
     // This was a callback request from Yahoo, get the token
-    $yahooService->requestAccessToken(
-        $_GET['oauth_token'],
-        $_GET['oauth_verifier'],
-        $token->getRequestTokenSecret()
-    );
+    var_dump($token); die();
     // Send a request now that we have access token
     $result = json_decode($yahooService->request('profile'));
     echo 'result: <pre>' . print_r($result, true) . '</pre>';
 } elseif (!empty($_GET['go']) && $_GET['go'] === 'go') {
-    // extra request needed for oauth1 to request a request token :-)
-    $token = $yahooService->requestRequestToken();
-    $url = $yahooService->getAuthorizationUri(array('oauth_token' => $token->getRequestToken()));
+    $url = $yahooService->getAuthorizationUri();
     header('Location: ' . $url);
 } else {
     $url = $currentUri->getRelativeUri() . '?go=go';
