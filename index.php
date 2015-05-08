da8d039d32a1027b740dc2a1fe9cedb00faf8fb3
Azeroz <nguoichet001@yahoo.com>
2015-05-08 07:28:29 +0700

    Fix spelling error

--- index.php
+++ index.php
@@ -1,7 +1,37 @@
  <?php 
 	if (isset($_COOKIE['is_logged_in'])){
-		echo '<p>Thank you for coming back!</p>';
-		echo '<p>Redirect in 3s</p>';
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<meta charset="utf-8">
+ 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+ 	<title>Welcome back!</title>
+ 	<link href='//fonts.googleapis.com/css?family=Roboto+Slab:400,700,300&subset=latin,vietnamese' rel='stylesheet' type='text/css'>
+ 	<link rel="stylesheet" href="assets/ui/semantic.css">
+ 	<link rel="stylesheet" href="assets/css/main.css">
+ </head>
+ <body class="vertical-center">
+ 	<div class="container">
+ 		<div class="ui grid">
+ 			<div class="row">
+ 				<div class="column">
+	 				<div class="ui header centered">
+	 					<h1 class="ui header" style="margin-bottom: 1em;">Welcome back!</h1>
+	 				</div>
+	 				<div class="return-box text-center">
+	 					<p>Thank you for coming back!</p>
+	 					<p>Redirect in 3s</p>
+	 				</div>
+ 				</div>
+ 			</div>
+ 		</div>
+ 	</div>
+ </body>
+ </html>
+
+ <?php
 		$url = 'success.php';
 		header('refresh: 3; url='.$url);
 	}
